f2502c4ce28b40e70b8e1a47a96f6c40cb2961a0
MITS Labs <73417721+mits-lab@users.noreply.github.com>
2024-05-07 10:24:21 -0400

    Update nj1-installcomet.ps1
    Added directory check function and adjusted formatting.

--- nj1-installcomet.ps1
+++ nj1-installcomet.ps1
@@ -1,19 +1,46 @@
 # NinjaOne Script to Install Comet backup
 # 
-# version 1.01
+# version 1.03
 #
+
 # Download installer to Local directory
+
 $DownloadURL = "https://files.monocleitsolutions.com/s/5yWoGYq6Neeej6y/download/DATACAGE%20LITE%2024.2.3.zip"
-$DownloadLocation = "C:\MITSCB\2024.2.3\" 
+$DownloadLocation = "C:\MITSCB\2024.2.3\"
+
 # Test DownloadLocation to make sure that nothing is overwritten, create folder if missing, and download zip from file server
-$TestDownloadLocation = Test-Path $DownloadLocation 
-if(!$TestDownloadLocation){
-new-item $Downloadlocation -ItemType Directory -force
-Invoke-Webrequest -Uri $DownloadURL -OutFile "$($DownloadLocation)\DATACAGELITE_24.2.3.zip" 
+
+function TempPath
+{
+    if(Test-Path $DownloadLocation)
+    {
+        Remove-Item "$DownloadLocation\*";
+    }
+    else
+    {
+        New-Item $DownloadLocation -ItemType Directory -force;
+    }
 }
+
+TempPath;
+
+# Download application package to $DownloadLocation
+
+Invoke-Webrequest -Uri $DownloadURL -OutFile "$($DownloadLocation)\DATACAGELITE_24.2.3.zip" 
+
+# Specify the pwd for the running shell to permit the installer to access its local .dat file.
+
+Set-Location -Path $DownloadLocation
+
 # Unzip the app
+
 Expand-Archive -Path "$DownloadLocation\DATACAGELITE_24.2.3.zip" -DestinationPath "$DownloadLocation\"
+
 # Executing msiexec command to install Comet backup using unique username and password for each user
+
 Start-Process "$DownloadLocation\install.exe" -ArgumentList "/S", "/LOBBY", "/SHORTCUT=disable", /TRAYICON="disable"
+
+# Log app installation
+
 $date = Get-Date | Out-String
 Ninja-Property-Set CometbackupInstalled $date
